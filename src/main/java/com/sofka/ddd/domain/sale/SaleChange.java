1b8c88734473a0b92dad7018565d1f68794716da
Sofia Idarraga <sofia.idarraga@udea.edu.co>
2022-08-18 16:40:16 -0500

    all events applyed

--- src/main/java/com/sofka/ddd/domain/sale/SaleChange.java
+++ src/main/java/com/sofka/ddd/domain/sale/SaleChange.java
@@ -2,7 +2,13 @@ package com.sofka.ddd.domain.sale;
 
 import co.com.sofka.domain.generic.EventChange;
 import com.sofka.ddd.domain.sale.events.BaristaAdded;
+import com.sofka.ddd.domain.sale.events.BaristaEntranceHourChanged;
+import com.sofka.ddd.domain.sale.events.BaristaNameUpdated;
+import com.sofka.ddd.domain.sale.events.BaristaOutHourChanged;
 import com.sofka.ddd.domain.sale.events.ClientAdded;
+import com.sofka.ddd.domain.sale.events.ClientContactNumberUpdated;
+import com.sofka.ddd.domain.sale.events.ClientNameUpdated;
+import com.sofka.ddd.domain.sale.events.ClientVipStatusUpdated;
 import com.sofka.ddd.domain.sale.events.DateOfSaleChanged;
 import com.sofka.ddd.domain.sale.events.ProductPriceUpdated;
 import com.sofka.ddd.domain.sale.events.ProductTypeChanged;
@@ -73,6 +79,42 @@ public class SaleChange extends EventChange {
             var product = sale.getProductById(event.getProductID())
                     .orElseThrow(()->new IllegalArgumentException("Product ID doesn't exist"));
             product.changeType(event.getType());
+        });
+
+        apply((ClientNameUpdated event)->{
+            var client = sale.getClientById(event.getClientID())
+                    .orElseThrow(()-> new IllegalArgumentException("Client ID doesn't exist"));
+            client.updateName(event.getName());
+        });
+
+        apply((ClientContactNumberUpdated event)->{
+            var client = sale.getClientById(event.getClientID())
+                    .orElseThrow(()-> new IllegalArgumentException("Client ID doesn't exist"));
+            client.updateContactNumber(event.getContactNumber());
+        });
+
+        apply((ClientVipStatusUpdated event)->{
+            var client = sale.getClientById(event.getClientID())
+                    .orElseThrow(()-> new IllegalArgumentException("Client ID doesn't exist"));
+            client.updateVipStatus(event.getVipStatus());
+        });
+
+        apply((BaristaNameUpdated event)->{
+            var barista = sale.getBaristaById(event.getBaristaID())
+                    .orElseThrow(()-> new IllegalArgumentException("Barista ID doesn't exist"));
+            barista.updateName(event.getName());
+        });
+
+        apply((BaristaEntranceHourChanged event)->{
+            var barista = sale.getBaristaById(event.getBaristaID())
+                    .orElseThrow(()-> new IllegalArgumentException("Barista ID doesn't exist"));
+            barista.changeEntranceHour(event.getEntranceHour());
+        });
+
+        apply((BaristaOutHourChanged event)->{
+            var barista = sale.getBaristaById(event.getBaristaID())
+                    .orElseThrow(()-> new IllegalArgumentException("Barista ID doesn't exist"));
+            barista.changeEntranceHour(event.getOutHour());
         });
     }
 
